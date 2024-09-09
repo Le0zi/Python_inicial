934df928d87e6e6c92a77ad73b507069eeebe862
Leonardo Machado Britto Junior <leozinnjr8@gmail.com>
2024-09-09 12:18:58 +0000

    Exercicio de casa

---
+++ Função Python/Exercicio_casa_01_09_09.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls||clear")
+
+def inflacao(num:float):
+    if num<100:
+        numif = num+(num*0.1)
+    else:
+        numif = num+(num*0.2)
+    return numif
+    
+
+
+produto=float(input("Informe o valor do produto: "))
+valor=inflacao(produto)
+print(f"Valor inflacionado: {valor}R$")
